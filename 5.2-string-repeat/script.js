bc614085d202c10e879dac7f85bef0c43793ece4
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-23 02:58:26 +0300

    5.2

---
+++ 5.2-string-repeat/script.js
@@ -0,0 +1,6 @@
+function repeatString(num, str) {
+  return str.repeat(num);
+}
+
+console.log('repeatString(3, "Hello")', repeatString(3, "Hello"));
+console.log('repeatString(6, "l")', repeatString(6, "l"));
