04909762ccdd3f1f0375a4916bf2acd6801d613c
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-23 02:52:17 +0300

    5.1

---
+++ 5.1-trimming-string/script.js
@@ -0,0 +1,6 @@
+function trimString(str){
+  return str.substring(1, str.length-1);
+}
+
+console.log('trimString("ASMAR")', trimString("ASMAR"));
+console.log('trimString("Power")', trimString("Power"));
